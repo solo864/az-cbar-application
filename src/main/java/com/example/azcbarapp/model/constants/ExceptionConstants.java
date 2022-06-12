093c2900e54bef99910e57807e92c899b6f6b74e
iamusayev <iamusayev1337@gmail.com>
2022-06-12 16:50:52 +0400

    Added exception constants

---
+++ src/main/java/com/example/azcbarapp/model/constants/ExceptionConstants.java
@@ -0,0 +1,13 @@
+package com.example.azcbarapp.model.constants;
+
+public class ExceptionConstants {
+
+    public static final String UNEXPECTED_EXCEPTION_CODE = "UNEXPECTED_EXCEPTION";
+    public static final String UNEXPECTED_EXCEPTION_MESSAGE = "Unexpected exception occurred";
+
+    public static final String CLIENT_EXCEPTION_MESSAGE = "an error occurred with cbar client";
+
+    public static final String RATE_EXCEPTION_CODE = "RATE_EXCEPTION_CODE";
+    public static final String RATE_EXCEPTION_MESSAGE = "This information with the specified date already exists in the database";
+
+}
