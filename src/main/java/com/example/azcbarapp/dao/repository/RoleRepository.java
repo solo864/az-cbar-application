45b959aa44be7c0ad5fa6ffd0318650c97397a6b
iamusayev <iamusayev1337@gmail.com>
2022-06-12 16:50:01 +0400

    Added role repository

---
+++ src/main/java/com/example/azcbarapp/dao/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.azcbarapp.dao.repository;
+
+
+import com.example.azcbarapp.dao.entity.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+
+}
