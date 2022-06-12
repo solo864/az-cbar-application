a27eac3d376157e16ceeb252560300f7c6e944a2
iamusayev <iamusayev1337@gmail.com>
2022-06-12 16:48:03 +0400

    Added rate controller

---
+++ src/main/java/com/example/azcbarapp/controller/RateController.java
@@ -0,0 +1,44 @@
+package com.example.azcbarapp.controller;
+
+import com.example.azcbarapp.model.criteria.PageCriteria;
+import com.example.azcbarapp.model.criteria.RateCriteria;
+import com.example.azcbarapp.model.dto.PageableRateDto;
+import com.example.azcbarapp.service.RateService;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.jni.Local;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/rates")
+public class RateController {
+
+    private final RateService rateService;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public void save(LocalDate date) {
+        rateService.save(date);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteRateByDate(Local date) {
+        rateService.deleteByDate(date);
+    }
+
+    @GetMapping
+    public PageableRateDto getRates(RateCriteria rateCriteria, PageCriteria pageCriteria) {
+        return rateService.getRates(rateCriteria, pageCriteria);
+    }
+
+
+}
+
