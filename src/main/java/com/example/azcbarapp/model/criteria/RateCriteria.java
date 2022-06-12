83ec49fc8bb778feecda9602ee2414c35b300c23
iamusayev <iamusayev1337@gmail.com>
2022-06-12 16:51:27 +0400

    Added rate criteria

---
+++ src/main/java/com/example/azcbarapp/model/criteria/RateCriteria.java
@@ -0,0 +1,13 @@
+package com.example.azcbarapp.model.criteria;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RateCriteria {
+
+    private String date;
+    private String currency;
+
+}
